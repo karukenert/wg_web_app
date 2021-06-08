095ddb9b578dd2a7dc5f849bc7819be53da1afe0
Kenert Karu <karukenert@gmail.com>
2021-06-08 19:36:26 +0300

    added bootstrap & navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,39 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Word Guesser</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/about">About</router-link>
+            </li>
+          </ul>
+          <form class="d-flex">
+            <router-link class="nav-link" to="/login">Login</router-link>
+            <button class="btn btn-outline-primary" type="submit">
+              Register
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+    <br />
+
     <router-view />
   </div>
 </template>
