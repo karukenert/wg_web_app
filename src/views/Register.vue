b62499a40d1e3703f4d79bb01fe0bf60b215c06a
Kenert Karu <karukenert@gmail.com>
2021-06-09 21:10:49 +0300

    register page

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,8 +1,40 @@
 <template>
   <div class="container">
+    <h1>Register</h1>
+
+    <br />
     <div class="row">
       <div class="col-md-3"></div>
-      <div class="col-md-6">Registration page</div>
+      <div class="col-md-6">
+        <form>
+          <div class="mb-3">
+            <input
+              type="text"
+              class="form-control"
+              id="fullName"
+              placeholder="Full name"
+            />
+          </div>
+          <div class="mb-3">
+            <input
+              type="email"
+              class="form-control"
+              id="email"
+              placeholder="Email address"
+            />
+          </div>
+          <div class="mb-3">
+            <input
+              type="password"
+              class="form-control"
+              id="poassword"
+              placeholder="Password"
+            />
+          </div>
+          <br />
+          <button type="submit" class="btn btn-primary">Sign up</button>
+        </form>
+      </div>
       <div class="col-md-3"></div>
     </div>
   </div>
