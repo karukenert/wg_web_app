a803226008cf4ce5df4ab3dcda36d6d433214d32
Kenert Karu <karukenert@gmail.com>
2021-07-28 17:47:39 +0300

    moved login from vue file to userservice

--- src/services/userService.js
+++ src/services/userService.js
@@ -4,6 +4,25 @@ import * as Constants from '../constants.js';
 
 // TODO log out method
 
+export async function logIn(username, password) {
+    const response = await axios.post(`${Constants.BASE_URL}/user/login`, {
+        username,
+        password
+    });
+
+    if (response.status === 200) {
+        store._actions.LOGGED_IN_SET[0]({
+            accessToken: response.data.accessToken,
+            refreshToken: store.getters.refreshToken,
+        });
+        return true;
+    } else {
+        // TODO Error modal
+        alert(`We have an error: ${response.data}`);
+        return false;
+    }
+}
+
 export async function syncLoggedIn() {
     const response = await axios.post(`${Constants.BASE_URL}/user/token`, {
         accessToken: store.getters.getAccessToken,
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Login</h1>
-    <form @submit.prevent="login">
+    <form @submit.prevent="logUserIn">
       <input
         type="text"
         class="form-control"
@@ -25,9 +25,8 @@
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex";
-import axios from "axios";
-import * as Constants from "../constants.js";
+import { mapGetters } from "vuex";
+const userService = require("@/services/userService");
 
 export default {
   name: "login",
@@ -36,23 +35,9 @@ export default {
     password: "",
   }),
   methods: {
-    ...mapActions(["LOGGED_IN_SET"]),
-    async login() {
-      const response = await axios.post(`${Constants.BASE_URL}/user/login`, {
-        username: this.username,
-        password: this.password,
-      });
-
-      if (response.status === 200) {
-        await this.LOGGED_IN_SET({
-          accessToken: response.data.accessToken,
-          refreshToken: response.data.refreshToken,
-        });
-        this.$router.push("/");
-      } else {
-        // TODO Error modal
-        alert(`We have an error: ${response.data}`);
-      }
+    async logUserIn() {
+      let status = await userService.logIn(this.username, this.password);
+      if (status) this.$router.push("/");
     },
   },
   computed: {
