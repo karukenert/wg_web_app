4b0d4be2a8c67c870704d8bf76653272f1654342
Kenert Karu <karukenert@gmail.com>
2021-07-31 20:15:45 +0300

    #5 - loading component

---
+++ src/components/Loading.vue
@@ -0,0 +1,19 @@
+<template>
+  <img src="@/assets/Spinner-1.2s-91px.svg" />
+</template>
+
+<style scoped>
+img {
+  text-align: center;
+  animation: rotation 1s infinite linear;
+}
+
+@keyframes rotation {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(359deg);
+  }
+}
+</style>
