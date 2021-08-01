571f669f6b899f99d56181f5eeae597ffd6ff41c
Kenert Karu <karukenert@gmail.com>
2021-08-01 16:27:49 +0300

    using loading component in different views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,14 @@
     <div class="layout" v-if="this.isLoggedIn">
       <div class="top">
         <div class="column-header"><h3>Search for a word!</h3></div>
-        <form @submit.prevent="fetchWord">
+        <form @submit.prevent="fetchWord" v-if="!loading">
           <div>
             <input type="text" v-model="this.queryWord" />
           </div>
 
           <button type="submit">Search</button>
         </form>
+        <Loading v-else />
       </div>
 
       <div class="left">
@@ -28,25 +29,36 @@
 
 <script>
 import { mapGetters } from "vuex";
-import { getWord } from "@/services/wordService";
+import { getWord } from "@/services/wordService.js";
 import WordCard from "@/components/WordCard.vue";
+import Loading from "@/components/Loading.vue";
 
 export default {
   name: "Home",
   data: () => ({
     queryWord: "",
     word: {},
+    loading: false,
   }),
   components: {
     WordCard,
+    Loading,
   },
   computed: {
     ...mapGetters(["isLoggedIn", "getTokens"]),
   },
   methods: {
     async fetchWord() {
+      this.loading = true;
       // If empty word, show error
-      this.word = await getWord(this.queryWord, this.getTokens);
+      let response = await getWord(this.queryWord, this.getTokens);
+
+      if (response === false) {
+        console.log("no word found");
+      } else {
+        this.word = response;
+      }
+      this.loading = false;
     },
   },
 };
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Login</h1>
-    <form @submit.prevent="logUserIn">
+    <form @submit.prevent="logUserIn" v-if="!loading">
       <input
         type="text"
         class="form-control"
@@ -21,26 +21,31 @@
 
       <button type="submit">Log in</button>
     </form>
+    <Loading v-else />
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from "vuex";
+import Loading from "@/components/Loading.vue";
 const userService = require("@/services/userService");
 
 export default {
+  components: { Loading },
   name: "login",
   data: () => ({
     username: "",
     password: "",
+    loading: false,
   }),
   methods: {
     ...mapActions(["LOGGED_IN_SET"]),
     async logUserIn() {
-      // TODO implement loading.
+      this.loading = true;
       let logInStatus = await userService.logIn(this.username, this.password);
       if (logInStatus.status) {
         await this.LOGGED_IN_SET(logInStatus.tokens);
+        this.loading = false;
         this.$router.push("/");
       } else {
         //TODO  show error modal
