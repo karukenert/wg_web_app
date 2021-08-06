1766eb3b163e06cefcd90ff76caf6a430433e603
Kenert Karu <karukenert@gmail.com>
2021-08-06 23:54:47 +0200

    refactor login styles

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,27 +1,31 @@
 <template>
   <div>
-    <h1>Login</h1>
-    <form @submit.prevent="logUserIn" v-if="!loading">
-      <input
-        type="text"
-        class="form-control"
-        id="username"
-        v-model="username"
-        required
-        placeholder="Username"
-      />
-      <input
-        type="password"
-        class="form-control"
-        id="password"
-        v-model="password"
-        required
-        placeholder="Password"
-      />
+    <div class="container">
+      <div><h1>Login</h1></div>
+      <div v-if="!loading">
+        <form @submit.prevent="logUserIn">
+          <input
+            type="text"
+            class="form-control"
+            id="username"
+            v-model="username"
+            required
+            placeholder="Username"
+          />
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            v-model="password"
+            required
+            placeholder="Password"
+          />
 
-      <button type="submit">Log in</button>
-    </form>
-    <Loading v-else />
+          <button type="submit">Log in</button>
+        </form>
+      </div>
+      <div v-else><Loading /></div>
+    </div>
   </div>
 </template>
 
@@ -58,5 +62,23 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.container {
+  width: fit-content;
+
+  button {
+    background-color: aqua;
+    margin: 8px;
+    padding: 8px 12px;
+    border-radius: 50px;
+    border-color: aqua;
+    font-weight: bold;
+    transition: 0.5s;
+
+    &:hover {
+      cursor: pointer;
+      border-color: black;
+    }
+  }
+}
 </style>
